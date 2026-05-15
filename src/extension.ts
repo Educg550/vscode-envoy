c1754ce69154c86c2a94a723d60e3f4914416159
Educg550 <eduardo9cruz@gmail.com>
2026-05-15 00:26:08 -0300

    feat(extension): register URI handler for vscode:// deep links

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,19 @@
 import * as vscode from 'vscode';
 import { shareFileCommand } from './commands/shareFile';
-import { openNoteCommand } from './commands/openNote';
+import { openNoteCommand, openNoteFromUrl } from './commands/openNote';
 
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
     vscode.commands.registerCommand('enclosed.shareFile', (uri?: vscode.Uri) => shareFileCommand(uri, context)),
     vscode.commands.registerCommand('enclosed.openNote', openNoteCommand),
+    vscode.window.registerUriHandler({
+      handleUri(uri: vscode.Uri): void {
+        if (uri.path === '/open') {
+          const url = new URLSearchParams(uri.query).get('url');
+          if (url) { void openNoteFromUrl(url); }
+        }
+      },
+    }),
   );
 }
 
